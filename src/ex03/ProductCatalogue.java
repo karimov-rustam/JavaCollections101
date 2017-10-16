0d696bbaab284df038ebeaea0882d3f2c797bea6
Karimov R <rkarimov.r@gmail.com>
2017-10-17 00:30:13 +0400

    Write ProductCatalogue with TreeSet implementation

--- src/ex03/ProductCatalogue.java
+++ src/ex03/ProductCatalogue.java
@@ -1,11 +1,13 @@
 package ex03;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
+
+import static ex03.Product.BY_NAME;
 
 public class ProductCatalogue implements Iterable<Product> {
-    private Set<Product> products = new HashSet<>();
+    private final Set<Product> products = new TreeSet<>(BY_NAME);
 
     void isSuppliedBy(Supplier supplier) {
         products.addAll(supplier.products());
